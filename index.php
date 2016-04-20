9db2bdcde528eb9624e6f0b2576808d85f414347
a1420216 <a1420216@192.168.160.15>
2016-04-20 20:18:13 -0300

    fazendo css

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>teste</title>
+        <link href="projeto1.css" type="text/css" rel="stylesheet" />
     </head>
     <body>
         <?php
-             
+            echo 'bens vindos DWE'; 
         ?>
     </body>
 </html>
